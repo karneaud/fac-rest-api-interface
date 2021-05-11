28e2d199ad0093bd2d8cdc6e6e027487a5d5501c
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-05-11 17:12:33 +0000

    add api route

---
+++ Modules/FAC/Routes/api.v1.php
@@ -0,0 +1,22 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+| @version 1.0.0
+*/
+
+$router->group(['prefix' => 'v1', 'namespace' => 'API\v1' ], function ($router) {
+   $router->get('/', function() use ($router) {
+   		return $router->app->version() . ' Module:FAC API Route version 1';
+   });
+
+	$router->post('/authorize', 'FACController@authorize' );
+});
