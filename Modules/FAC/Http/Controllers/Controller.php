1c0725b47d4402d028aff95e73a4d222ea55605c
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-05-11 17:14:44 +0000

    refactor to be modulebase controller

---
+++ Modules/FAC/Http/Controllers/Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Modules\FAC\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Contracts\Support\Renderable;
+use App\Http\Controllers\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    /**
+    * Create a new controller instance.
+    *
+    * @return void
+    */
+    public function __construct()
+    {
+    }
+}
