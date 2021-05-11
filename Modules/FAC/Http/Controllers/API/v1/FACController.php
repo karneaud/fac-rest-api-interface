1f271179956bbb133435a14b23c6b3d82542c584
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-05-11 22:28:52 +0000

    setup base methods

--- Modules/FAC/Http/Controllers/API/v1/FACController.php
+++ Modules/FAC/Http/Controllers/API/v1/FACController.php
@@ -7,7 +7,23 @@ use Modules\FAC\Http\Controllers\Controller as BaseController;
 
 class FACController extends BaseController
 {
+
+	/**
+	 * Authorizes 
+	 */
 	public function authorize(Request $request) {
     	
+    }
+
+	public function capture(Request $request) {
+    	
+    }
+
+	public function authorizeCapture(Request $request) {
+    	
+    }
+
+	public function refund(Request $request) {
+    
     }
 }
