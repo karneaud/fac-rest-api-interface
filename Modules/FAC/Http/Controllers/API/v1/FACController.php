32d85fbd568d59f8fa4fdd2df83ab60a2529cb5c
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-05-11 17:13:12 +0000

    add ver 1 controller

---
+++ Modules/FAC/Http/Controllers/API/v1/FACController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\FAC\Http\Controllers\API\v1;
+
+use Illuminate\Http\Request;
+use Modules\FAC\Http\Controllers\Controller as BaseController;
+
+class FACController extends BaseController
+{
+	public function authorize(Request $request) {
+    	
+    }
+}
