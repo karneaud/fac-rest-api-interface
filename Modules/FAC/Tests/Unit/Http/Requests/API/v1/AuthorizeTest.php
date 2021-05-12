c9b028d28cd095d20e6393bbe166b9574121b92c
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-05-12 04:45:32 +0000

    update tests

--- Modules/FAC/Tests/Unit/Http/Requests/API/v1/AuthorizeTest.php
+++ Modules/FAC/Tests/Unit/Http/Requests/API/v1/AuthorizeTest.php
@@ -2,21 +2,13 @@
 
 namespace Modules\FAC\Tests\Unit\Http\Requests\API\v1;
 
-use PHPUnit\Framework\TestCase;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Foundation\Testing\WithFaker;
 use Modules\FAC\Http\Requests\API\v1\Authorize;
+use Modules\FAC\Tests\Unit\UnitTestCase as TestCase;
 
 class AuthorizeTest extends TestCase
 {
-    protected $data = ['order_id' => 1, 
-            			'amount' => 10.00, 
-        				'currency' => "AUD",
-        				'card' => 'A12901290',
-        					'cvv' => 123, 
-        				'expiry_month' => '02', 
-        				'expiry_year' => '02'
-                      ];
+    
 	/**
      * test invalid validation 
      *
@@ -25,7 +17,7 @@ class AuthorizeTest extends TestCase
     public function testInValidRequests()
     {
         $request = new Authorize();
-    	$validator = Validator::make($this->data, $request->rules());
+    	$validator = Validator::make($this->getAuthorizeRequestData(), $request->rules());
 
     	$this->assertFalse($validator->passes());
     	$this->assertContains('card', $arr = $validator->errors()->keys(), "Card was not validated " . join("<br/>", $arr));
@@ -40,7 +32,7 @@ class AuthorizeTest extends TestCase
     public function testValidRequests()
     {
         $request = new Authorize();
-    	$validator = Validator::make(array_merge($this->data, [
+    	$validator = Validator::make($this->getAuthorizeRequestData([
         	'currency' => 'TTD',
         	'card' => 12345678901234,
         	]), $request->rules());
