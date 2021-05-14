39e1e473a80ab4e4aa416451af563c211bcc3d98
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-05-14 05:49:09 +0000

    refactor method name. added service class property

--- Modules/FAC/Http/Controllers/API/v1/FACController.php
+++ Modules/FAC/Http/Controllers/API/v1/FACController.php
@@ -3,30 +3,44 @@
 namespace Modules\FAC\Http\Controllers\API\v1;
 
 use Illuminate\Http\Request;
+use Modules\FAC\Services\API\v1\FACService;
 use Modules\FAC\Http\Requests\API\v1\Authorize;
 use Modules\FAC\Http\Controllers\Controller as BaseController;
 
 class FACController extends BaseController
 {
+	protected $service;
 
+	public function __construct() {
+    	$this->service = new FACService(
+        				env('FAC_MERCHANT_ID','123456789'), 
+        				env('FAC_MERCHANT_PASSWD','abc123'), 
+        				false,
+        				env('FAC_TEST_MODE', false));
+    }
 	/**
-	 * Sends authourization requests to FAC Authorize operation
-	 * @method sendAuthorizeRequest
-	 * @param Illuminate\Http\Request $request Request input 
-	 */
-	public function sendAuthorizeRequest(Request $request) {
-    	return $this->validateRequest($request, new Authorize );
-    }
-
-	public function capture(Request $request) {
+	 * Will handle authorize/ capture scenarios
+	 * @method purchase
+	 * @param Illuminate\Http\Request $request the request parameters input
+	 * @return \Illuminate\Http\JsonResponse
+	 */ 
+	public function purchase(Request $request) {
     	
-    }
-
-	public function authorizeCapture(Request $request) {
-    	
-    }
-
-	public function refund(Request $request) {
-    
+    	$this->validateRequest($request, new Authorize);
+    	return $this->returnResponse(
+        	$this->service->purchase(
+            	array_merge(
+                	$request->only('card','amount','currency'), 
+                	['transactionId' => $request->input('order_id'),
+                     	'card' => [
+        					'number' => $request->input('card'), 
+        					'expiryMonth' => $request->input('expiry_month'), 
+        					'expiryYear' => $request->input('expiry_year'),
+        					'cvv' => $request->input('cvv'),
+        				]
+                   	]
+                		)
+            	)
+        );
     }
 }
