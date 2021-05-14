41008229a41e335172deb3aefc68dfcf5a2191d2
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-05-14 05:52:31 +0000

    refactored test

--- Modules/FAC/Tests/Unit/Http/Controllers/API/v1/FACControllerTest.php
+++ Modules/FAC/Tests/Unit/Http/Controllers/API/v1/FACControllerTest.php
@@ -2,20 +2,18 @@
 
 namespace Modules\FAC\Tests\Unit\Http\Controllers\API\v1;
 
+use Http\Mock\Client;
+use Modules\FAC\Http\Requests\API\v1\Authorize;
 use Modules\FAC\Tests\Unit\UnitTestCase as TestCase;
 use Modules\FAC\Http\Controllers\API\v1\FACController;
 
 class FACControllerTest extends TestCase
 {
-	protected $request;
-	protected function setUp() :void {
-    	$this->request = new \Illuminate\Http\Request($this->getAuthorizeRequestData());
-    }
     /**
-     * Test sendAuthorizeRequest method validation
+     * Test valideRequest method ValidationException
      * @return void
      */
-    public function testSendAuthorizeRequestValidateException()
+    public function testAuthorizeRequestValidateException()
     {
     	$this->expectException(\Illuminate\Validation\ValidationException::class);
     
@@ -26,17 +24,6 @@ class FACControllerTest extends TestCase
                  ->method('validateRequest')
         		 ->will($this->throwException(new \Illuminate\Validation\ValidationException('Validation Exception') ));
     
-    	$mock->sendAuthorizeRequest($this->request);
-    }
-
-	/**
-     * Test sendAuthorizeRequest method validation
-     * @return void
-     */
-    public function testSendAuthorizeRequestValidate()
-    {
-    	$controller = new FACController;
-    	$this->assertTrue($controller->sendAuthorizeRequest( new \Illuminate\Http\Request($this->getAuthorizeRequestData(['currency' => 'USD', 'card' => '123456789098765' ])) ));
-    	
+    	$mock->purchase( new \Illuminate\Http\Request( $this->getAuthorizeRequestData() ), new Authorize);
     }
 }
