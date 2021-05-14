f6a18cc590da5a94d8999406e2ccd42ab8c69130
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-05-14 05:53:13 +0000

    use trait instead

--- Modules/FAC/Tests/Unit/UnitTestCase.php
+++ Modules/FAC/Tests/Unit/UnitTestCase.php
@@ -4,20 +4,10 @@ namespace Modules\FAC\Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Modules\FAC\Tests\AuthorizeRequestDataTrait;
 
 class UnitTestCase extends TestCase {
-
-	protected function getAuthorizeRequestData($data = null ) {
-    	$defaults = ['order_id' => 1, 
-            			'amount' => 10.00, 
-        				'currency' => "AUD",
-        				'card' => 'A12901290',
-        				'cvv' => 123, 
-        				'expiry_month' => '02', 
-        				'expiry_year' => '02'
-                      ];
-    	if(!is_null($data)) $defaults = array_merge($defaults, $data ); 
-    
-    	return $defaults;
-    }
+	
+	use AuthorizeRequestDataTrait;
+	
 }
