c30f549ecea934d133f41a96976e12c744b2c833
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-05-16 18:16:18 +0000

    add response code. add swagger comments

--- Modules/FAC/Http/Controllers/API/v1/FACController.php
+++ Modules/FAC/Http/Controllers/API/v1/FACController.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * @OA\Info(
+ *     version="1.0",
+ *     title="FAC Interface Platform endpoints"
+ * )
+ */
 namespace Modules\FAC\Http\Controllers\API\v1;
 
 use Illuminate\Http\Request;
@@ -19,16 +24,60 @@ class FACController extends BaseController
         				env('FAC_TEST_MODE', false));
     }
 	/**
-	 * Will handle authorize/ capture scenarios
+ 	 * @OA\Post(
+	 *     path="/fac/api/v1/purchase",
+	 *     description="make a purchase request",
+     *     operationId="purchase",
+ 	 *	   @OA\RequestBody(
+     *		   description="post request parameters for purchase",
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(ref="#/components/schemas/PurchaseRequest")
+     *         )
+     *     ),
+	 *     @OA\Response(
+	 * 		response="200",
+	 * 		description="Successful purchase",
+	 * 		@OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(ref="#/components/schemas/SuccessPurchaseResponse")
+	 * 		)
+	 *    ),
+	 *     @OA\Response(
+	 * 		response="503",
+	 * 		description="Unsuccessful purchase",
+	 * 		@OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(ref="#/components/schemas/FailPurchaseResponse")
+	 * 		)
+	 *    ),
+	 *     @OA\Response(
+	 * 		response="422",
+	 * 		description="Validation Error Response",
+	 * 		@OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     * 					type="object",
+     * 					title="Validation Error Response Object",
+     * 					@OA\Property( property="error", type="string", format="string", description="validation error message")
+     * 				)
+	 * 		)
+	 *    )
+	 * )
+ 	 */
+	/** 
+	 * Sends a one pass transaction request to FAC
 	 * @method purchase
-	 * @param Illuminate\Http\Request $request the request parameters input
-	 * @return \Illuminate\Http\JsonResponse
+	 * @params Illuminate\Http\Request $request POST request inputs
+	 * @return Illuminate\Http\JsonResponse $response response content as application/json
+	 * @throws Illuminate\Validation\ValidationException
 	 */ 
 	public function purchase(Request $request) {
     	
     	$this->validateRequest($request, new Authorize);
     	return $this->returnResponse(
-        	$this->service->purchase(
+        	$response = ($this->service->purchase(
             	array_merge(
                 	$request->only('card','amount','currency'), 
                 	['transactionId' => $request->input('order_id'),
@@ -40,7 +89,8 @@ class FACController extends BaseController
         				]
                    	]
                 		)
-            	)
+            	)),
+        	array_key_exists('error', $response)? 503 : 200
         );
     }
 }
