b4b5974c2a044115a435bb5f3837ac17e5801093
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-06-04 01:07:17 +0000

    update annotation

--- Modules/FAC/Annotations/v1/Annotation.php
+++ Modules/FAC/Annotations/v1/Annotation.php
@@ -28,7 +28,7 @@
  * 			description="Successful purchase",
  * 			@OA\MediaType(
  *             mediaType="application/json",
- *             @OA\Schema(ref="#/components/schemas/SuccessPurchaseResponse")
+ *             @OA\Schema(ref="#/components/schemas/SuccessResponse")
  * 			)
  *    	),
  *      @OA\Response(
@@ -36,7 +36,7 @@
  * 			description="Unsuccessful purchase",
  * 			@OA\MediaType(
  *             mediaType="application/json",
- *             @OA\Schema(ref="#/components/schemas/FailPurchaseResponse")
+ *             @OA\Schema(ref="#/components/schemas/FailResponse")
  * 			)
  *     ),
  *     @OA\Response(
@@ -61,24 +61,169 @@
  * 	  security={{"bearer": {} }}
  * )
  * 
+ * @OA\Post(
+ *		path="/v1/refund",
+ *		tags={"FAC"},
+ * 		@OA\Server(
+ * 			url="/fac/api",
+ * 			description="FAC module endpoints for the platform"
+ * 		),
+ *		description="send a refund request",
+ *     	operationId="refund",
+ *	   	@OA\RequestBody(
+ *		   description="post request parameters for purchase",
+ *         required=true,
+ *         @OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(ref="#/components/schemas/RefundRequest")
+ *         )
+ *    	 ),
+ *     	@OA\Response(
+ * 			response="200",
+ * 			description="Successful refund",
+ * 			@OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(ref="#/components/schemas/SuccessResponse")
+ * 			)
+ *    	),
+ *      @OA\Response(
+ * 			response="503",
+ * 			description="Unsuccessful purchase",
+ * 			@OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(ref="#/components/schemas/FailResponse")
+ * 			)
+ *     ),
+ *     @OA\Response(
+ * 			response="401",
+ * 			description="Unauthorized action",
+ * 			@OA\MediaType(
+ *             mediaType="text/html"
+ * 			)
+ *     ),
+ *     @OA\Response(
+ * 		response="422",
+ * 		description="Validation Error Response",
+ * 		@OA\MediaType(
+ *             mediaType="application/json",
+ *             @OA\Schema(
+ * 					type="object",
+ * 					title="Validation Error Response Object",
+ * 					@OA\Property( property="error", type="string", format="string", description="validation error message")
+ * 				)
+ * 		)
+ *    ),
+ * 	  security={{"bearer": {} }}
+ * )
+ *
  * @OA\Schema(
- *     title="Successful Purchase Response",
+ * 	   schema="PurchaseRequest",
+ *     title="Purchase Request",
+ * 	   required={"order_id,currency,amount,cvv,card"},
+ *	   type="object",
+ *     description="POST request parameters and validation constraints",
+ * 	   @OA\Property(
+ * 			property="order_id",
+ * 			type="string",
+ *     		format="string",
+ *     		title="Order Id",
+ *     		description="The order id of the purchase transaction"
+ * 	   ),
+ * 	   @OA\Property(
+ * 			property="currency",
+ * 			type="string",
+ * 			format="string",
+ *     		pattern="\w{3}",
+ *     		title="Currency",
+ *     		description="The 3 letter ISO country currency code for the amount"
+ * 	   ),
+ * 	   @OA\Property(
+ * 			property="amount",
+ * 			type="number",
+ * 			format="float",
+ *     		title="Amount",
+ *     		description="The amount total of the order"
+ * 	   ),
+ * 	   @OA\Property(
+ * 			property="card",
+ * 			type="string",
+ * 			format="string",
+ *     		title="Card Number",
+ *     		description="The credit card # or token PAN to use to process payment"
+ * 	   ),
+ * 	   @OA\Property(
+ * 			property="cvv",
+ * 			type="integer",
+ * 			format="integer",
+ *     		pattern="\d{3}",
+ *     		title="CCV",
+ *     		description="The 3 digit security code for the credit card"
+ * 	   ),
+ * 	   @OA\Property(
+ * 			property="expiry_month",
+ * 			type="integer",
+ * 			format="integer",
+ *     		pattern="\d{1,2}",
+ *     		title="Expiry Month",
+ *     		description="The expiration month on the credit card"
+ * 	   ),
+ * 	   @OA\Property(
+ * 			property="expiry_year",
+ * 			type="integer",
+ * 			format="integer",
+ *     		pattern="\d{4}",
+ *     		title="Expiry Year",
+ *     		description="The expiration year on the credit card"
+ * 	   ),
+ * 	   @OA\Property(
+ * 			property="tokenized",
+ * 			type="boolean",
+ * 			format="boolean",
+ *     		title="Card Number is a Token PAN",
+ *     		description="required if card number is a token PAN "
+ * 	   )
+ * )
+ *	
+* @OA\Schema(
+ * 	   schema="RefundRequest",
+ *     title="Refund Request",
+ * 	   required={"order_id,amount"},
+ *	   type="object",
+ *     description="POST request parameters and validation constraints",
+ * 	   @OA\Property(
+ * 			property="order_id",
+ * 			type="string",
+ *     		format="string",
+ *     		title="Order Id",
+ *     		description="The order id of the purchase transaction"
+ * 	   ),
+ * 	   @OA\Property(
+ * 			property="amount",
+ * 			type="number",
+ * 			format="float",
+ *     		title="Amount",
+ *     		description="The amount total of the order"
+ * 	   )
+ * )
+ *
+ * @OA\Schema(
+ *     title="Successful Response",
  * 	   type="object",
- * 	   schema="SuccessPurchaseResponse",
+ * 	   schema="SuccessResponse",
  *     description="Formatted success response from FAC",
  * 	   @OA\Property(
  * 		 property="order_id",
  * 		 type="string",
  *     	 format="string",
  *     	 title="Order Id",
- *     	 description="The order id of the purchase transaction"
+ *     	 description="The order id of the transaction"
  * 	   ),
  * 	   @OA\Property(
- * 		 property="trasnsaction_id",
+ * 		 property="transaction_id",
  * 		 type="string",
  * 		 format="string",
  *     	 title="Transaction ID",
- *     	 description="FAC Reference id of the transaction IF successful"
+ *     	 description="FAC Reference id of the transaction IF successful if available"
  * 	   ),
  * 	   @OA\Property(
  * 		 property="success",
@@ -92,28 +237,42 @@
  * 		 type="string",
  * 		 format="string",
  *     	 title="token PAN",
- *     	 description="FAC credit card token PAN IF process was successful"
+ *     	 description="FAC credit card token PAN IF requested and process is successful and not already tokenized"
+ * 	   ),
+ * 	   @OA\Property(
+ * 		 property="code",
+ * 		 type="integer",
+ * 		 format="integer",
+ *     	 title="Message Code",
+ *     	 description="Code for response"
+ * 	   ),
+ * 	   @OA\Property(
+ * 		 property="message",
+ * 		 type="string",
+ * 		 format="string",
+ *     	 title="Message",
+ *     	 description="Message for response"
  * 	   )
  * )
  * 
  * @OA\Schema(
- *     title="Failed Purchase Response",
+ *     title="Failed Response",
  * 	   type="object",
- * 	   schema="FailPurchaseResponse",
+ * 	   schema="FailResponse",
  *     description="Formatted error response from FAC",
  * 	   @OA\Property(
  * 		 property="order_id",
  * 		 type="string",
  *     	 format="string",
  *     	 title="Order Id",
- *     	 description="The order id of the purchase transaction"
+ *     	 description="The order id of the transaction if available"
  * 	   ),
  * 	   @OA\Property(
  * 			property="trasnsaction_id",
  * 			type="string",
  * 			format="string",
  *     		title="Transaction ID",
- *     		description="Reference id of the transaction IF successful"
+ *     		description="Reference id of the transaction IF available"
  * 	   ),
  * 	   @OA\Property(
  * 			property="error",
