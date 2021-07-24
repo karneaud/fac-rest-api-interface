93352e832043741763027dd9cd0f891b3b810497
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-07-24 15:48:25 +0000

    use signature

--- Modules/FAC/Http/Middleware/VerifyRequest.php
+++ Modules/FAC/Http/Middleware/VerifyRequest.php
@@ -65,7 +65,7 @@ class VerifyRequest
         	if(!$error) throw new \Exception('Unverified request!'); 
         	
         } catch(\Exception $e) {
-        	Log::alert("{$e->getMessage()}  for KEY #{$user->api_key} with request ");
+        	Log::alert("{$e->getMessage()}  for KEY #{$user->api_key} with request " . $request->header('Signature') );
         	return response($e->getMessage(), 401);
         }
     
